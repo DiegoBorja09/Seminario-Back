58493f1265a2d4ddcf931dbe91b0f7a246d02269
DiegoBorja09 <diegoborja09@gmail.com>
2023-01-16 22:58:40 -0500

    commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
 const { dbUsername, dbPassword, dbHost, dbName } = require(".")
 
+mongoose.set("strictQuery", false);
 const connection= async function(){
     const conn = await mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`)
     console.log("Mongo DB connected:",conn.connection.host) 
